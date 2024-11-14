71d3faa49281351e60220d6e7866020864edc853
Vijay Krishan <vk856549@gmail.com>
2024-11-14 11:02:33 +0530

    modify path

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import {  RouterProvider  } from 'react-router-dom'
-import { router } from './Routes/Routes'
+
+import { RouterProvider } from 'react-router-dom';
+import router from './Routes/Routes';
+
 
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={router}/>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App
